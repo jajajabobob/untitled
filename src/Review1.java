bc3a6e598cc0c134001e433c29a2e2e7bfc7bf6e
mitani taiga <241001@std.hi-joho.ac.jp>
2025-05-14 10:02:08 +0900

    新規

--- src/Review1.java
+++ src/Review1.java
@@ -1,8 +1,8 @@
 public class Review1 {
     public static void main(String[] args){
         System.out.print("こんにちは");
-        System.out.printl("今日の天気は　 +    晴れです");
-        Sysem.out.print("今日は一日"　\n" 洗濯日和です。");
+        System.out.println("今日の天気は + 晴れです");
+        System.out.print("今日は一日"+" 洗濯日和です。");
         System.out.println("外に干しても大丈夫そうです");
         System.out.println("三谷大河");
     }
