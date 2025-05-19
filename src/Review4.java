9626396f561f2f32eb075616acb656f8f5da8dc7
mitani taiga <241001@std.hi-joho.ac.jp>
2025-05-19 11:50:47 +0900

    20280519

--- src/Review4.java
+++ src/Review4.java
@@ -1,5 +1,14 @@
 public class Review4 {
     public static void main(String[] args){
+        int number1 = 5;//1number  number1
+        String str = 88 + "88";
+        double PI = 3.14159265358979323846;//final
+        PI = 3.14;//final
+        long a = 1;//入りきらない
+        String  flg = "true" ;
+        byte b = 35;//
+        int num = 0x735;//
+        char c = 'a';//
 
     }
 }
